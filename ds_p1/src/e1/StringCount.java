408659527be7e081487de49cde8237016895df0d
Meleagrista <75792048+Meleagrista@users.noreply.github.com>
2021-10-07 09:02:20 +0200

    Update StringCount.java

--- ds_p1/src/e1/StringCount.java
+++ ds_p1/src/e1/StringCount.java
@@ -2,54 +2,55 @@ package e1;
 
 public class StringCount {
 
+    //String of the elements we consider symbols
     private static final String CMP = "?@#$.,";
 
     public static int countWords(String text) {
-        int count = 0;
-        if(text == null){ return count;}
+        int count = 0; //counter
+        if(text == null){ return count;} //if there is no text return 0
         char[] ch = new char[text.length()];
         for (int i = 0; i < text.length(); i++) {
             ch[i] = text.charAt(i);
-            if (((i > 0) && (ch[i] != ' ') && (ch[i - 1] == ' ')) || ((ch[0] != ' ') && (i == 0)))
+            if (((i > 0) && (ch[i] != ' ') && (ch[i - 1] == ' ')) || ((ch[0] != ' ') && (i == 0))) //check for any kind of space
                 count++;
         }
         return count;
     }
 
     public static int countChar(String text, char c) {
-        int count = 0;
-        if(text == null){ return count;}
+        int count = 0; //counter
+        if(text == null){ return count;} //if there is no text return 0
         for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == c)
+            if (text.charAt(i) == c) //check for the same character
                 count++;
         }
         return count;
     }
 
     public static int countCharIgnoringCase(String text, char c) {
-        int count = 0;
+        int count = 0; //counter
         if(text == null){ return count;}
         for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == Character.toUpperCase(c) || text.charAt(i) == Character.toLowerCase(c))
+            if (text.charAt(i) == Character.toUpperCase(c) || text.charAt(i) == Character.toLowerCase(c)) //check for the same character in upper and lower case
                 count++;
         }
         return count;
     }
 
     public static boolean isPasswordSafe(String password) {
-        boolean upper = false, lower = false, digit = false, symbol = false;
+        boolean upper = false, lower = false, digit = false, symbol = false; //variables to check if... there is an upper, lower, digit and symbol
 
-        if (password.length() < 8)
+        if (password.length() < 8) //is too short
             return false;
         for (int i = 0; i < password.length(); i++) {
-            if (Character.isLowerCase(password.charAt(i)))
+            if (Character.isLowerCase(password.charAt(i))) //there is a lower case
                 lower = true;
-            if (Character.isUpperCase(password.charAt(i)))
+            if (Character.isUpperCase(password.charAt(i))) //there is a upper case
                 upper = true;
-            if (Character.isDigit(password.charAt(i)))
+            if (Character.isDigit(password.charAt(i))) //there is a digit
                 digit = true;
             for (int j = 0; j < CMP.length(); j++) {
-                if (CMP.charAt(j) == password.charAt(i))
+                if (CMP.charAt(j) == password.charAt(i)) //there is a symbol
                 {
                     symbol = true;
                     break;
@@ -58,18 +59,4 @@ public class StringCount {
         }
         return lower && upper && digit && symbol;
     }
-
-    public void main(String[] args) {
-        //String str1 ="A password like this is not safe";
-        String str2 ="A password containing 1 symbol like # this is safe";
-        char character = 'a';
-        System.out.println(countWords(str2) + " words.");
-        System.out.println(countChar(str2,character) + " characters found.");
-        System.out.println(countCharIgnoringCase(str2,character) + " characters found.");
-        if (isPasswordSafe(str2))
-            System.out.println("The password is safe");
-        else
-            System.out.println("The password is not safe");
-    }
-
 }
