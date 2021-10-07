5915921c29d93c38949eb78b19ef5eedfb485a6f
Meleagrista <75792048+Meleagrista@users.noreply.github.com>
2021-10-07 09:13:01 +0200

    Update Slopes.java

--- ds_p1/src/e2/Slopes.java
+++ ds_p1/src/e2/Slopes.java
@@ -5,9 +5,9 @@ package e2;
 public class Slopes {
 
     public static int downTheSlope ( char [][] slopeMap , int right , int down ) {
-        int i, j;
-        int counter = 0;
-        int x = 0, y = 0;
+        int i, j; //counters
+        int counter = 0; //number of crashes
+        int x = 0, y = 0; //counter in the axis X and Y
         //NON SQUARE MAPS
         for( i = 0; i < slopeMap.length; i++ ){ if (slopeMap[i].length != slopeMap.length) { throw new IllegalArgumentException(); } }
         //NON VALID CHARACTERS
@@ -19,26 +19,26 @@ public class Slopes {
         //NON VALID PARAMETERS
         if( (right > 10) || (down > 10) || (right < 1) || (down < 1) ) { throw new IllegalArgumentException(); }
 
-        if( slopeMap[y][x] == '#' ) { counter++; }
-        while( y < slopeMap.length ){
+        if( slopeMap[y][x] == '#' ) { counter++; } //check the first
+        while( y < slopeMap.length ){ //the limit is the Y axis
             for(j = 0; j < right; j++) {
-                x++;
-                if( x == slopeMap[y].length ) { x = 0; }
-                if( slopeMap[y][x] == '#' ) { counter++; }
+                x++; //advance
+                if( x == slopeMap[y].length ) { x = 0; } //if you surpass the X axis...
+                if( slopeMap[y][x] == '#' ) { counter++; } //check
             }
             for(i = 0; i < down; i++) {
-                y++;
-                if( y == slopeMap.length ) { return counter; }
-                if( slopeMap[y][x] == '#' ) { counter++; }
+                y++; //advance
+                if( y == slopeMap.length ) { return counter; } //check the last line
+                if( slopeMap[y][x] == '#' ) { counter++; } //check
             }
         }
         return counter;
     }
 
     public static int jumpTheSlope ( char [][] slopeMap , int right , int down ) {
-    int i, j;
-    int counter = 0;
-    int x = 0, y = 0;
+    int i, j; //counters
+    int counter = 0; //number of crashes
+    int x = 0, y = 0; //counter in the axis X and Y
     //NON SQUARE MAPS
         for( i = 0; i < slopeMap.length; i++ ){ if (slopeMap[i].length != slopeMap.length) { throw new IllegalArgumentException(); } }
     //NON VALID CHARACTERS
@@ -50,17 +50,17 @@ public class Slopes {
     //NON VALID PARAMETERS
         if( (right > 10) || (down > 10) || (right < 1) || (down < 1) ) { throw new IllegalArgumentException(); }
 
-        if( slopeMap[y][x] == '#' ) { counter++; }
+        if( slopeMap[y][x] == '#' ) { counter++; } //check the first
         while( y < slopeMap.length ){
         for(j = 0; j < right; j++) {
-            x++;
+            x++; //advance
             if( x == slopeMap[y].length ) { x = 0; }
         }
         for(i = 0; i < down; i++) {
-            y++;
-            if( y == slopeMap.length ) { return counter; }
+            y++; //advance
+            if( y == slopeMap.length ) { return counter; } //check the last line
         }
-        if( slopeMap[y][x] == '#' ) { counter++; }
+        if( slopeMap[y][x] == '#' ) { counter++; } //check
     }
         return counter;
 }
