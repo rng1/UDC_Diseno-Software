06524ec031ab72dc1754395bcec71da08ebce837
Martin <martin.rio.rico.do@gmail.com>
2021-12-14 15:53:12 +0100

    Started working on the main program.

--- DS-61-01-A3/src/e2/TaskPlanner.java
+++ DS-61-01-A3/src/e2/TaskPlanner.java
@@ -1,5 +1,6 @@
 package e2;
 
+import java.io.*;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -9,25 +10,18 @@ public class TaskPlanner {
     Queue<Character> queueTask;
     Map<Character, List<Character>> graphTask;
 
-    public TaskPlanner(){
+    public TaskPlanner() {
         queueTask = new PriorityQueue<>();
     }
 
-    //public void traverseGraph(file, criterion)
-    // {
-    //    try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-    //    String line;
-    //    while ((line = br.readLine()) != null) {
-    //       // process the line.
-    //    }
-    //      while map is not empty
-    //          while there is some avaible
-    //              criterion.isAvailable(char)
-    //              saveNode(char)
-    //          freeNode(char)
-    //
-    //}
+    public void planTask(File file, GraphIterator iterator) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // process the line
+            }
+        } catch (IOException e) { e.printStackTrace(); }
 
-
-
+        queueTask = iterator.traverseGraph(graphTask);
+    }
 }
